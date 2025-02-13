edc40ca3e2a02387673033cb17eac16e75e0b130
Hava Haviv <131984507+Hava980@users.noreply.github.com>
2025-02-13 02:18:01 +0200

    added main

--- TSS-ECDSA/CppImplementation/SignatureVerification.cpp
+++ TSS-ECDSA/CppImplementation/SignatureVerification.cpp
@@ -574,3 +574,24 @@ public:
         return verifier.verifySignature(message, signature);
     }
 };
+
+
+int main()
+{
+    ThresholdECDSA ecdsa(2, 3);
+    ecdsa.generateKeys();
+
+    std::vector<int> signingGroup = {1, 2};
+    std::string message = "hello world from threshold ecdsa";
+
+    std::cout << "Signing message: " << message << std::endl;
+    json signature = ecdsa.signMessage(message, signingGroup);
+
+    std::cout << "Generated signature: " << signature.dump(2) << std::endl;
+
+    bool isValid = ecdsa.verifySignature(message, signature);
+    std::cout << "Signature is " << (isValid ? "valid" : "invalid") << std::endl;
+
+    return 0;
+}
+
