8b1c4a35cc0ae891f0baeb0fad285fe16c11e82e
emanuel malloul <108261833+Emelloul98@users.noreply.github.com>
2025-03-01 12:26:53 +0200

    Create main.cpp

---
+++ TSS-ECDSA/openSSL/main.cpp
@@ -0,0 +1,11 @@
+#include <openssl/ecdsa.h>
+#include <openssl/obj_mac.h>
+#include <openssl/rand.h>
+#include <openssl/bn.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
