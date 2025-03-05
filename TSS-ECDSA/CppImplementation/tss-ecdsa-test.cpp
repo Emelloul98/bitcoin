1dfa8c4c54898a251222bfc20dda598f7a9be4cb
Hava Haviv <131984507+Hava980@users.noreply.github.com>
2025-03-05 18:40:08 +0200

    Update tss-ecdsa-test.cpp

--- TSS-ECDSA/CppImplementation/tss-ecdsa-test.cpp
+++ TSS-ECDSA/CppImplementation/tss-ecdsa-test.cpp
@@ -1,1 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <nlohmann/json.hpp>
+#include <openssl/bn.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
+#include <openssl/evp.h>
+#include <fstream>
+
+
+using json = nlohmann::json;
+
+struct Participant {
+    BIGNUM* ui;
+    BIGNUM* ki;
+    BIGNUM* wi;
+    BIGNUM* xi;
+
+    BIGNUM* gamma_i;
+    BIGNUM* sigma_i;
+    BIGNUM* delta_i;
+    BIGNUM* s_i;
+    EC_POINT* yi;
+    std::vector<BIGNUM*> polynomial;
+    std::vector<BIGNUM*> shares;
+    int participant_id;
+}
+
+ BIGNUM *generate_random_zq(BIGNUM *order)
+    {
+        BIGNUM *rand = BN_new();
+        BN_rand_range(rand, order);
+        return rand;
+    }
+
+BIGNUM *hash_message(const std::string &message)
+{
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    unsigned char hash[EVP_MAX_MD_SIZE];
+    unsigned int hash_len;
+
+    EVP_DigestInit_ex(ctx, EVP_sha256(), NULL);
+    EVP_DigestUpdate(ctx, message.c_str(), message.size());
+    EVP_DigestFinal_ex(ctx, hash, &hash_len);
+
+    BIGNUM *bnHash = BN_new();
+    BN_bin2bn(hash, hash_len, bnHash);
+
+    EVP_MD_CTX_free(ctx);
+    return bnHash;
+}
 
