5636bdccf54318d9e26fe2a3cb00f0f96365dace
emanuel malloul <108261833+Emelloul98@users.noreply.github.com>
2025-03-05 18:43:24 +0200

    implement evaluate_polynomial function

--- TSS-ECDSA/CppImplementation/threshold-ecdsa/thresholdECDSA.cpp
+++ TSS-ECDSA/CppImplementation/threshold-ecdsa/thresholdECDSA.cpp
@@ -1,1 +1,76 @@
+#include "ThresholdECDSA.hpp"
+
+
+BIGNUM *ThresholdECDSA::generate_random_zq()
+{
+    BIGNUM *res = BN_new();
+    BN_rand_range(res, order);
+    return res;
+}
+
+
+std::vector<BIGNUM *> ThresholdECDSA::generate_polynomial_t(BIGNUM *ui)
+{
+    std::vector<BIGNUM *> coefficients;
+    coefficients.push_back(BN_dup(ui));
+    for (int i = 1; i < t; i++)
+    {
+        coefficients.push_back(generate_random_zq());
+    }
+    return coefficients;
+}
+
+BIGNUM *ThresholdECDSA::evaluate_polynomial(const std::vector<BIGNUM *> &coefficients, int x)
+{
+    BIGNUM *result = BN_new();  
+    BIGNUM *temp = BN_new();    
+    BIGNUM *x_power = BN_new(); 
+    BN_CTX *ctx = BN_CTX_new(); 
+
+    BN_zero(result); 
+    BN_one(x_power); // x^0 = 1
+
+    for (size_t i = 0; i < coefficients.size(); i++)
+    {
+        // temp = coefficients[i] * x_power mod order
+        BN_mod_mul(temp, coefficients[i], x_power, order, ctx);
+
+        // result = result + temp mod order
+        BN_mod_add(result, result, temp, order, ctx);
+
+        // x_power = x_power * x mod order
+        BN_mul_word(x_power, x); // x_power *= x
+    }
+
+    BN_free(temp);
+    BN_free(x_power);
+    BN_CTX_free(ctx);
+
+    return result;
+}
+
+ThresholdECDSA:: ThresholdECDSA(int threshold, int total_participants)
+    : t(threshold), n(total_participants)
+{
+    group = EC_GROUP_new_by_curve_name(NID_secp256k1);
+    order = BN_new();
+    EC_GROUP_get_order(group, order, nullptr);
+    publicKey = EC_POINT_new(group);
+    
+   
+    //if (!EC_POINT_copy(publicKey, EC_GROUP_get0_generator(group))) {
+    //    std:: cout << "ctxv";
+    //}
+
+    EC_POINT_set_to_infinity(group, publicKey);
+    generator = EC_POINT_new(group);
+    EC_POINT_copy(generator, EC_GROUP_get0_generator(group));
+
+    for (int i = 1; i <= n; i++)
+    {
+        Participant* data = new Participant();
+        generate_participant_data(i, *data);
+        participants.push_back(data);
+    }
+}
 
