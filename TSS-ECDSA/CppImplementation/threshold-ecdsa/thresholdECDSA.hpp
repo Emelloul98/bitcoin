e96251ebb1e72c58ea280dc9c12e110d2e8c6d69
emanuel malloul <108261833+Emelloul98@users.noreply.github.com>
2025-03-18 12:04:13 +0200

    Update thresholdECDSA.hpp

--- TSS-ECDSA/CppImplementation/threshold-ecdsa/thresholdECDSA.hpp
+++ TSS-ECDSA/CppImplementation/threshold-ecdsa/thresholdECDSA.hpp
@@ -1,5 +1,5 @@
-#ifndef THRESHOLDECDSA_HPP
-#define THRESHOLDECDSA_HPP
+#ifndef SIMPLEECDSA_HPP
+#define SIMPLEECDSA_HPP
 
 #include <vector>
 #include <string>
@@ -10,54 +10,40 @@
 #include <openssl/evp.h>
 #include <openssl/sha.h>
 #include <openssl/err.h>
-#include <nlohmann/json.hpp>
+#include <cstring>
 
-using json = nlohmann::json;
 
 struct Participant
 {
-    BIGNUM *u;
+    BIGNUM *y;
     BIGNUM *k;
-    BIGNUM *w;
     BIGNUM *x;
 
-    BIGNUM *gamma;
     BIGNUM *sigma;
-    BIGNUM *delta;
     BIGNUM *s;
-    EC_POINT *y;
-    std::vector<BIGNUM *> shares;
+    BIGNUM *gamma;
     int participant_id;
 
 
     Participant() {
-        u = BN_new();
+        y = BN_new();
         k = BN_new(); 
-        w = BN_new();
         x = BN_new();
-        gamma = BN_new();
         sigma = BN_new();
-        delta = BN_new();
         s = BN_new();
-        y = nullptr;
+        gamma = BN_new();
+        BN_one(gamma);
         participant_id = -1;
      }
 
      ~Participant() {
-        BN_free(u);
+        BN_free(y);
         BN_free(k);
-        BN_free(w);
         BN_free(x);
-        BN_free(gamma);
         BN_free(sigma);
-        BN_free(delta);
         BN_free(s);
-        if (y) EC_POINT_free(y);
-        if (!shares.empty()) {
-        for (auto share : shares) {
-                BN_free(share);
-            }
-        }
+        BN_free(gamma);
+        
      }
 };
 
@@ -72,34 +58,142 @@ struct Signature
     //   BN_free(s);
     //}
 };
-class ThresholdECDSA {
+
+
+class simpleECDSA {
 private:
     const EC_GROUP *group;
     BIGNUM *order;
     EC_POINT *generator;
     EC_POINT *publicKey;
+    BN_CTX *ctx;
+
     int t;
     int n;
     std::vector<Participant*> participants;
 
-    BIGNUM* generate_random_zq();
-    std::vector<BIGNUM *> generate_polynomial_t(BIGNUM *ui);
-    BIGNUM *evaluate_polynomial(const std::vector<BIGNUM *> &coefficients, int x);
-    void generate_participant_data(int participant_id, Participant &data);
-    void generateSignatureShares(const std::vector<int> &signingGroup);
-    ECDSA_SIG * combineSignatureShares(const std::vector<int> &signingGroup, const BIGNUM *messageHash);
-    void saveToFile(const std::string &filename);
+    BIGNUM *H(const std::string& input);
+    BIGNUM *H0(const EC_POINT* point);
 
+    BIGNUM* generate_random_zq();
+    std::vector<BIGNUM *> generate_polynomial_t(BIGNUM *x);
+    BIGNUM *evaluate_polynomial(const std::vector<BIGNUM *> &coefficients, int x);
+    void generate_participants_data(const std::vector<int> &signingGroup);
+    EC_POINT* computeR(const std::vector<int> &signingGroup, BIGNUM* delta_inv);
+    void compute_sigma(std::vector<int> signingGroup);
+
+    // void partialSignature(int participant_id, const BIGNUM *messageHash);
+    // ECDSA_SIG *combineSignature(const std::vector<int> &signingGroup);
 
 public:
-    ThresholdECDSA(int threshold, int total_participants);
-    EC_POINT * generateKeys();
-    ECDSA_SIG * signMessage(const char *message, size_t message_length, const std::vector<int> &signingGroup,BIGNUM* msgHash);
-    //bool verifySignature(const std::string &messageHex, const json &signature);
-    ~ThresholdECDSA();
+    simpleECDSA(int threshold, int total_participants);
+    void generateKeys();
+    Signature* signMessage(const std::string& message, const std::vector<int> &signingGroup);
+    bool verifySignature(const std::string& message, Signature* signature);
+//    ~simpleECDSA();
 
 };
 
-#endif // THRESHOLDECDSA_HPP
+#endif // SIMPLEECDSA_HPP
+
+
+
+
+
+// #ifndef SIMPLEECDSA_HPP
+// #define SIMPLEECDSA_HPP
+
+// #include <vector>
+// #include <string>
+// #include <fstream>
+// #include <iostream>
+// #include <openssl/bn.h>
+// #include <openssl/ec.h>
+// #include <openssl/evp.h>
+// #include <openssl/sha.h>
+// #include <openssl/err.h>
+// #include <cstring>
+
+
+// struct Participant
+// {
+//     BIGNUM *y;
+//     BIGNUM *k;
+//     BIGNUM *x;
+
+//     BIGNUM *sigma;
+//     BIGNUM *s;
+//     BIGNUM *gamma;
+//     int participant_id;
+
+
+//     Participant() {
+//         y = BN_new();
+//         k = BN_new(); 
+//         x = BN_new();
+//         sigma = BN_new();
+//         s = BN_new();
+//         gamma = BN_new();
+//         BN_one(gamma);
+//         participant_id = -1;
+//      }
+
+//      ~Participant() {
+//         BN_free(y);
+//         BN_free(k);
+//         BN_free(x);
+//         BN_free(sigma);
+//         BN_free(s);
+//         BN_free(gamma);
+        
+//      }
+// };
+
+// struct Signature
+// {
+//     BIGNUM *r;
+//     BIGNUM *s;
+
+//     //~Signature() TODO
+//     //{
+//     //    BN_free(r);
+//     //   BN_free(s);
+//     //}
+// };
+
+
+// class simpleECDSA {
+// private:
+//     const EC_GROUP *group;
+//     BIGNUM *order;
+//     EC_POINT *generator;
+//     EC_POINT *publicKey;
+//     BN_CTX *ctx;
+
+//     int t;
+//     int n;
+//     std::vector<Participant*> participants;
+
+//     BIGNUM *H(const std::string& input);
+//     BIGNUM *H0(const EC_POINT* point);
+
+//     BIGNUM* generate_random_zq();
+//     std::vector<BIGNUM *> generate_polynomial_t(BIGNUM *x);
+//     BIGNUM *evaluate_polynomial(const std::vector<BIGNUM *> &coefficients, int x);
+//     void generate_participants_data(const std::vector<int> &signingGroup);
+//     EC_POINT* computeR(const std::vector<int> &signingGroup, BIGNUM* delta_inv);
+//     void compute_sigma(std::vector<int> signingGroup);
+
+
+// public:
+//     simpleECDSA(int threshold, int total_participants);
+//     void generateKeys();
+//     Signature* signMessage(const std::string& message, const std::vector<int> &signingGroup);
+//     bool verifySignature(const std::string& message, Signature* signature);
+// //    ~simpleECDSA();
+
+// };
+
+// #endif // SIMPLEECDSA_HPP
 
 
