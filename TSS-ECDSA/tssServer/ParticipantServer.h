bbfaad65f9720b9c9a4a7f52d62725186f54bfef
emanuel malloul <108261833+Emelloul98@users.noreply.github.com>
2025-05-10 17:59:04 +0300

    Create ParticipantServer.h

---
+++ TSS-ECDSA/tssServer/ParticipantServer.h
@@ -0,0 +1,19 @@
+#ifndef PARTICIPANTSERVER_H
+#define PARTICIPANTSERVER_H
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <openssl/bn.h>
+#include <openssl/rand.h>
+#include <openssl/ec.h>
+extern EC_GROUP* group;
+extern BIGNUM* order;
+extern BN_CTX* ctx;
+
+void run_server(int port);
+
+#endif //PARTICIPANTSERVER_H
